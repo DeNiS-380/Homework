fee0e313f1728d06469e55d8613a6ce068e44be3
den-i <den.iv.495@gmail.ru>
2024-11-10 19:17:24 +0300

    10-2/11

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string = input("Введите свою фамилию,имя: ")
+print(
+    f"{my_string},{len(my_string)}, {my_string.upper()}, {my_string.lower()}, {my_string.replace(" ", "")}, {my_string[0]}, {my_string[-1]}.")
+print(len(my_string))
+print(my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
